42fe0005f88d03537f694bb927e6d3f0fe696e47
itc-n24011 <167495721+itc-n24011@users.noreply.github.com>
2024-07-18 15:37:30 +0900

    Add files via upload

---
+++ Q4_9_2.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+MAX_VALUE = 2
+
+print(sys.getdefaultencoding())
+print(os.path.basename(os.getcwd()))
+
+for i in range(3):
+    print(i, end=" ")
+    if MAX_VALUE > i:
+        print(MAX_VALUE)
+    else:
+        print("#")
+
